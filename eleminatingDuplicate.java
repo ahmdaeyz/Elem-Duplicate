3177409ec29355f7e09ac1532216b30b24973ed6
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2018-05-30 20:38:34 +0200

    Long live the concurrency gods

--- eleminatingDuplicate.java
+++ eleminatingDuplicate.java
@@ -1,65 +1,115 @@
-import java.io.*;
-import javax.swing.filechooser.FileFilter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.security.MessageDigest;
+import java.io.File;
 import java.io.FileInputStream;
-public class eleminatingDuplicate{
+import java.security.MessageDigest;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-    public static void main(String[] args) throws Exception{
-        List<File> files = new ArrayList();
+public class Main {
+
+
+    public static class DuplicateCandidate {
+
+        public DuplicateCandidate(File file) {
+            this.file = file;
+            this.hash = checkSumForFile(file);
+            System.out.println(Thread.currentThread().getName() + " - " + file.getName() + " : " + hash);
+        }
+
+        public final File file;
+        public final String hash;
+        public boolean deleted = false;
+    }
+
+    public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the path to the Directory : ");
         String dirPath = input.nextLine();
-        flist(dirPath,files);
-        System.out.println("Number of Elements in the Directory : "+files.size());
-        String[] hashes = new String[files.size()];
-        int[] counter = new int[files.size()];
-        for(int i =0;i<files.size();i++){
-          hashes[i]=checkSumForFile(files.get(i));
+
+        long startTime = System.currentTimeMillis();
+
+        List<File> files = new ArrayList();
+        flist(dirPath, files);
+
+        System.out.println("Number of Elements in the Directory : " + files.size());
+
+
+        List<DuplicateCandidate> duplicateCandidates = Collections.synchronizedList(new ArrayList<DuplicateCandidate>());
+
+
+        int MAX_THREAD_NUMBER = 32;
+
+        Thread[] threads = new Thread[MAX_THREAD_NUMBER];
+
+        AtomicInteger counter = new AtomicInteger(-1);
+        for (int i = 0; i < MAX_THREAD_NUMBER; i++) {
+            threads[i] = new Thread(() -> {
+                int index;
+                while ((index = counter.incrementAndGet()) < files.size()) {
+                    duplicateCandidates.add(new DuplicateCandidate(files.get(index)));
+                }
+            });
+            threads[i].setName("THREAD - " + i);
+            threads[i].start();
         }
-        for(int i =0;i<files.size();i++){
-            for(int j=i+1;j<files.size();j++){
-                if(hashes[i].equals(hashes[j])){
-                files.get(i).delete();
-                files.remove(i);
-                System.out.println("A Duplicate Got Deleted !");
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+
+        for (int i = 0; i < duplicateCandidates.size(); i++) {
+            System.out.println("Searching for duplicates for :" + duplicateCandidates.get(i).file);
+            for (int j = i + 1; j < duplicateCandidates.size(); j++) {
+                if (!duplicateCandidates.get(j).deleted &&
+                        duplicateCandidates.get(i).hash.equalsIgnoreCase(duplicateCandidates.get(j).hash)) {
+                    System.out.println("\tDuplicate found :" + duplicateCandidates.get(j).file );
+                    duplicateCandidates.get(j).deleted = true;
+                    //duplicateCandidates.get(j).file.delete();
                 }
             }
         }
-        System.out.println("Number of Elements in the Directory : "+files.size());
+
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.println(estimatedTime);
     }
-    public static void flist(String directoryName ,List<File> files){
+
+    public static void flist(String directoryName, List<File> files) {
         File dir = new File(directoryName);
         File[] fls = dir.listFiles();
-        for(File file:fls){
-            if(file.isFile()){
-                if(get(file,file.getName())){
+        for (File file : fls) {
+            if (file.isFile()) {
                 files.add(file);
-                }
-            }else if(file.isDirectory()){
-                flist(file.getAbsolutePath(),files);
+            } else if (file.isDirectory()) {
+                flist(file.getAbsolutePath(), files);
             }
         }
     }
-    public static boolean get(File file,String name){
-        return name.toLowerCase().endsWith(".jpg");
+
+
+    public static String checkSumForFile(File file) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA1");
+            FileInputStream fileInput = new FileInputStream(file);
+            byte[] dataBytes = new byte[1024];
+            int bytesRead = 0;
+            while ((bytesRead = fileInput.read(dataBytes)) != -1) {
+                messageDigest.update(dataBytes, 0, bytesRead);
+            }
+            byte[] digestBytes = messageDigest.digest();
+            StringBuffer sb = new StringBuffer("");
+            for (int i = 0; i < digestBytes.length; i++) {
+                sb.append(Integer.toString((digestBytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            fileInput.close();
+
+
+            return sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
-    public static String checkSumForFile(File file) throws Exception {
-        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-	    FileInputStream fileInput = new FileInputStream(file);
-	    byte[] dataBytes = new byte[1024];
-	    int bytesRead = 0;
-	    while ((bytesRead = fileInput.read(dataBytes)) != -1){
-	         messageDigest.update(dataBytes, 0, bytesRead);
-	    }
-	    byte[] digestBytes = messageDigest.digest();
-	    StringBuffer sb = new StringBuffer("");
-	    for (int i = 0; i < digestBytes.length; i++) {
-	        sb.append(Integer.toString((digestBytes[i] & 0xff) + 0x100, 16).substring(1));
-	    }
-        fileInput.close();
-        return sb.toString();
-    }
+
 }
